d8a74369f472bf7f46c5006e76f1e20f63a9e4c1
Vesa Kuokka <vkuokka@student.hive.fi>
2020-08-19 18:40:10 +0300

    Webserver created using flask.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
+from flask import Flask, render_template, session
+from flask_session import Session
+
+app = Flask(__name__)
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
+
 class Package:
 	def __init__(self):
 		self.name = None
 		self.description = None
 		self.depends = []
 		self.rdepends = []
-	def __str__(self):
-		return(self.name + '\n' + str(self.depends) + '\n' + str(self.rdepends) + '\n' + self.description)
 
 def parse_depends(line):
 	names = []
@@ -13,6 +19,7 @@ def parse_depends(line):
 	for separate in separated:
 		names.append(separate.split(' ')[0])
 	return names
+
 def search_rdepends(packages_object):
 	for package1 in packages_object:
 		name = getattr(package1, 'name')
@@ -20,8 +27,9 @@ def search_rdepends(packages_object):
 		for package2 in packages_object:
 			if name in getattr(package2, 'depends'):
 				rdepends.append(getattr(package2, 'name'))
-def main():
-	file = open('status.real', 'r')
+
+def create_packages():
+	file = open('../status.real', 'r')
 	packages_raw = file.read().split('\n\n')
 	packages_object = []
 	for package_raw in packages_raw:
@@ -36,14 +44,22 @@ def main():
 			elif name_field[0] == 'Description':
 				setattr(package, 'description', name_field[1])
 			elif name_field[0][0] == ' ':
-				setattr(package, 'description', str(getattr(package, 'description')) + ' \n' + name_field[0])
+				setattr(package, 'description', str(getattr(package, 'description')) + name_field[0])
 			elif name_field[0] == 'Depends':
 				setattr(package, 'depends', parse_depends(name_field[1]))
 		packages_object.append(package)
 	search_rdepends(packages_object)
-	for package in packages_object:
-			print(package)
 	file.close()
+	packages_object.sort(key=lambda package: package.name)
+	return packages_object
 
-if __name__ == '__main__':
-	main()
+@app.route('/')
+def display_all():
+	session['packages'] = create_packages()
+	return render_template('index.html', packages=session['packages'])
+@app.route('/<string:name>')
+def display_info(name):
+	for package in session['packages']:
+		if getattr(package, 'name') == name:
+			return render_template('info.html', package=package)
+	return 'Error :('
