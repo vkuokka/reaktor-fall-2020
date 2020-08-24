c9ce7b32a56e9f1dd8c70d60bd9af01941b67a11
Vesa Kuokka <vkuokka@student.hive.fi>
2020-08-24 15:37:19 +0300

    Refactor and error detection changes.

--- app/main.py
+++ app/main.py
@@ -4,31 +4,32 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 class Package:
-	def __init__(self, name, depends, description):
+	def __init__(self, name, dependencies, description):
 		self.name = name
-		self.depends = depends
+		self.dependencies = dependencies
 		self.description = description
-		self.rdepends = []
+		self.reverseDependencies = []
 
 	def __str__(self):
-		return str(self.name) + '\n' + str(self.description) + '\n' + str(self.depends) + '\n' + str(self.rdepends)
+		return str(self.name) + '\n' + str(self.description) + '\n' \
+		+ str(self.dependencies) + '\n' + str(self.reverseDependencies)
 
-def parse_depends(depends):
+def parse_dependencies(dependencies):
 	names = []
-	if depends == None:
+	if dependencies == None:
 		return names
-	for separatedByPipe in depends.split(' | '):
+	for separatedByPipe in dependencies.split(' | '):
 		for separatedByComma in separatedByPipe.split(', '):
 			names.append(separatedByComma.split(' ')[0])
 	return names
 
-def search_rdepends(packages_object):
+def search_reverse_dependencies(packages_object):
 	for package1 in packages_object:
 		name = getattr(package1, 'name')
-		rdepends = getattr(package1, 'rdepends')
+		reverseDependencies = getattr(package1, 'reverseDependencies')
 		for package2 in packages_object:
-			if name in getattr(package2, 'depends'):
-				rdepends.append(getattr(package2, 'name'))
+			if name in getattr(package2, 'dependencies'):
+				reverseDependencies.append(getattr(package2, 'name'))
 
 def regex_packages():
 	try:
@@ -38,31 +39,36 @@ def regex_packages():
 			file = open('status.real', 'r')
 		except IOError:
 			raise SystemExit
-	regex = []
-	regex.append(re.compile(r'^Package:\s(.+?(?=\n\S))', re.MULTILINE | re.DOTALL))
-	regex.append(re.compile(r'^Depends:\s(.+?(?=\n\S))', re.MULTILINE | re.DOTALL))
-	regex.append(re.compile(r'^Description:\s(.+?(?=\n\S))', re.MULTILINE | re.DOTALL))
 	packages_raw = file.read().split('\n\n')
+	file.close()
+	regexList = [
+	re.compile(r'^Package:\s(.+?(?=\n\S))', re.MULTILINE | re.DOTALL),
+	re.compile(r'^Depends:\s(.+?(?=\n\S))', re.MULTILINE | re.DOTALL),
+	re.compile(r'^Description:\s(.+?(?=\n\S))', re.MULTILINE | re.DOTALL)
+	]
 	packages_object = []
 	for package in packages_raw:
-		if package == '':
-			continue
-		package += '\nend'
-		found = []
-		for entry in regex:
-			match = entry.search(package)
-			if match:
-				found.append(match.group(1))
-			else:
-				found.append(None)
-		packages_object.append(Package(found[0], parse_depends(found[1]), found[2]))
-	search_rdepends(packages_object)
+		if 'Package: ' in package:
+			package += '\nEOF'
+			values = []
+			for regexObject in regexList:
+				regexMatch = regexObject.search(package)
+				if regexMatch:
+					values.append(regexMatch.group(1))
+				else:
+					values.append(None)
+			packages_object.append(Package(values[0], parse_dependencies(values[1]), values[2]))
+	search_reverse_dependencies(packages_object)
 	packages_object.sort(key=lambda package: package.name)
-	file.close()
 	return packages_object
 
 global packages_object
 packages_object = regex_packages()
+
+# if __name__ =='__main__':
+# 	packages = regex_packages()
+# 	for package in packages:
+# 		print(package)
 
 @app.route('/')
 def display_all():
